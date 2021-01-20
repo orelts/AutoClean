7c4e9193844a11ca82d1d8567f969a7f13f9353d
Orel Tsioni <oreltsioni@gmail.com>
2021-01-20 02:18:56 +0200

    sql initiate modified with a function to print sql data for debug and querry + cleanup

--- sql_initiate.py
+++ sql_initiate.py
@@ -4,43 +4,75 @@ sql server is Microsoft SQL server.
 """
 
 import pyodbc
-server = 'localhost' # Replace this with the actual name of your SQL Edge Docker container
-username = 'sa' # SQL Server username
-password = 'ItamarOrel1995' # Replace this with the actual SA password from your deployment
-# database = 'Master' # Replace this with the actual database name from your deployment. If you do not have a database created, you can use Master database.
-db_connection_string = "Driver={FreeTDS};Server=" + server + ";port=1433" + ";UID=" + username + ";PWD=" + password + ";"
-conn = pyodbc.connect(db_connection_string,  autocommit=True)
 
 
-## pyodcb handle for accessing and acting on the server
-cursor = conn.cursor()
+## printing updated sql data in a dict way (Column, row). optional to choose table name
+def print_sql_row(curs ,table_name = "SensorsInfo"):
+    table_rows = curs.execute("SELECT * FROM " + table_name)
+    columns = [column[0] for column in curs.description]
+    print(columns)
+    for row in table_rows:
+        for idx in range(len(columns)):
+            print(columns[idx], row[idx])
 
-cursor.execute("CREATE DATABASE AutoCleanDB")
-cursor.execute("USE AutoCleanDB")
 
-## initiation of SQL for Sensors module
-cursor.execute('''
+## initiation sql SensorsInfo table (create if not created and insert initial values)
+def init_table(curs):
 
-if not exists (select * from sysobjects where name='SensorsInfo' and xtype='U')
-	CREATE TABLE dbo.SensorsInfo
-	(
-	  [ID] int NOT NULL PRIMARY KEY CLUSTERED
-	  , [groundspeed] varchar(100) 
-	  , [home_location] varchar(100) 
-	  , [battery] varchar(100)  
-	  , [last_heartbeat] varchar(100) 
-	  , [ValidFrom] datetime2 GENERATED ALWAYS AS ROW START
-	  , [ValidTo] datetime2 GENERATED ALWAYS AS ROW END
-	  , PERIOD FOR SYSTEM_TIME (ValidFrom, ValidTo)
-	 )
-	WITH (SYSTEM_VERSIONING = ON (HISTORY_TABLE = dbo.SensorsInfoHistory));
+    curs.execute('''
+    if not exists (select * from sysobjects where name='SensorsInfo' and xtype='U')
+        CREATE TABLE dbo.SensorsInfo
+        (
+          [ID] int NOT NULL PRIMARY KEY CLUSTERED
+          , [groundspeed] varchar(100) 
+          , [home_location] varchar(100) 
+          , [battery] varchar(100)  
+          , [last_heartbeat] varchar(100) 
+          , [ValidFrom] datetime2 GENERATED ALWAYS AS ROW START
+          , [ValidTo] datetime2 GENERATED ALWAYS AS ROW END
+          , PERIOD FOR SYSTEM_TIME (ValidFrom, ValidTo)
+         )
+        WITH (SYSTEM_VERSIONING = ON (HISTORY_TABLE = dbo.SensorsInfoHistory));
+    ''')
 
-''')
+    groundspeed_per = 0
+    home_location_per = 0
+    battery_per = 0
+    last_heartbeat_per = 0
 
-for row in cursor.tables():
-    print(row.table_name)
+    sensors_inf = (str(groundspeed_per), str(home_location_per), str(battery_per), str(last_heartbeat_per),
+                   str(groundspeed_per), str(home_location_per), str(battery_per), str(last_heartbeat_per))
+    cursor.execute('''
+                   IF NOT EXISTS (SELECT * FROM  dbo.SensorsInfo WHERE ID = 1) 
 
-conn.commit()
+                       INSERT INTO dbo.SensorsInfo(ID, groundspeed, home_location, battery, last_heartbeat)
+                       VALUES(1, ?, ?, ?, ?)
+
+                   ELSE
+
+                       UPDATE dbo.SensorsInfo
+                       SET groundspeed = ?, home_location = ?, battery = ?, last_heartbeat= ?
+                       WHERE ID = 1 ''', sensors_inf)
+
+
+if __name__ == '__main__':
+
+    server = 'localhost'
+    username = 'sa'
+    password = 'ItamarOrel1995'
+    # database = 'Master' # Replace this with the actual database name from your deployment. If you do not have a database created, you can use Master database.
+
+    db_connection_string = "Driver={FreeTDS};Server=" + server + ";port=1433" + ";UID=" + username + ";PWD=" + password + ";"
+
+    conn = pyodbc.connect(db_connection_string,  autocommit=True)
+    cursor = conn.cursor()
+
+    # cursor.execute("CREATE DATABASE AutoCleanDB")
+    cursor.execute("USE AutoCleanDB")
+
+    init_table(cursor)
+    print_sql_row(cursor)
+
 
 
 
