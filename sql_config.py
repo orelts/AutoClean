f847d883722e2f361c77f73ced1d990c2b2624ee
Orel Tsioni <oreltsioni@gmail.com>
2021-01-26 04:50:55 +0200

    supports generic sql init function for generic table alligned to a given dict

--- sql_config.py
+++ sql_config.py
@@ -7,11 +7,14 @@ d_sensors = {
 }
 
 
-def drop_table(cursor, conn, table, history_table = None):
-    cursor.execute('''ALTER TABLE dbo.''' + table + ''' SET (SYSTEM_VERSIONING = OFF)
-                      DROP TABLE AutoCleanDB.dbo.''' + table)
-    if history_table is not None:
-        cursor.execute(''' DROP TABLE AutoCleanDB.dbo.''' + history_table)
+def drop_table(cursor, conn, table, history_table=None):
+
+    if history_table is None:
+        cursor.execute('''DROP TABLE AutoCleanDB.dbo.''' + table)
+    else:
+        cursor.execute(''' ALTER TABLE dbo.''' + table + ''' SET (SYSTEM_VERSIONING = OFF)
+                      DROP TABLE AutoCleanDB.dbo.''' + table + '''
+                      DROP TABLE AutoCleanDB.dbo.''' + history_table)
 
     conn.commit()
 
@@ -27,65 +30,92 @@ def init_database(curs, conn):
 
 
 ## initiation sql SensorsInfo table (create if not created and insert initial values)
-def init__sensors_table(curs, conn):
-    sensors_sql_columns = ""
-    for key,value in d_sensors.items():
-        sensors_sql_columns += ", [" + key + "]" + " varchar(100)\n"
+def init_sql_table(curs, conn, table, sql_dict, is_one_row, history_table=None):
+    sql_columns = ""
+    for key, value in sql_dict.items():
+        sql_columns += ", [" + key + "]" + " varchar(100)\n"
+
+    if history_table is None:
+        history_str = ""
+    else:
+        history_str = "WITH (SYSTEM_VERSIONING = ON (HISTORY_TABLE = dbo." + history_table + "))"
+    if is_one_row:
+        incremental_id_str = ""
+    else:
+        incremental_id_str = "IDENTITY(1,1)"
 
     curs.execute('''
-    if not exists (select * from sysobjects where name='SensorsInfo' and xtype='U')
-        CREATE TABLE dbo.SensorsInfo
+    if not exists (select * from sysobjects where name=\'''' + table + '''\' and xtype='U')
+        CREATE TABLE dbo.''' + table + '''
         (
-          [ID] int NOT NULL PRIMARY KEY CLUSTERED
-          ''' + sensors_sql_columns + '''
+          [ID] int ''' + incremental_id_str + ''' NOT NULL PRIMARY KEY CLUSTERED
+          ''' + sql_columns + '''
           , [ValidFrom] datetime2 GENERATED ALWAYS AS ROW START
           , [ValidTo] datetime2 GENERATED ALWAYS AS ROW END
           , PERIOD FOR SYSTEM_TIME (ValidFrom, ValidTo)
          )
-        WITH (SYSTEM_VERSIONING = ON (HISTORY_TABLE = dbo.SensorsInfoHistory));
+        ''' + history_str + ''';
     ''')
-    conn.commit()
-
-    initial_sensors_info = [(v) for k, v in d_sensors.items()][0:]
-
-
-    sensors_data_line = ""
-    for key, value in d_sensors.items():
-        sensors_data_line += key + ","
-    sensors_data_line = sensors_data_line[:-1]
-
-
-    n = len(d_sensors)
-    values_str = ',?' * len(d_sensors)
-    curs.execute('''
-                   IF NOT EXISTS (SELECT * FROM  dbo.SensorsInfo WHERE ID = 1) 
-
-                       INSERT INTO dbo.SensorsInfo( ID, ''' + sensors_data_line + ''')
-                       VALUES(1''' + values_str + ''')
-                    ''', initial_sensors_info
-                )
 
     conn.commit()
 
+    initial_info = [v for k, v in sql_dict.items()][0:]
 
-def update_sensors_sql_row(curs, conn, data):
-    sensors_data_line = ""
+    data_line = ""
+    for key, value in sql_dict.items():
+        data_line += key + ","
+    data_line = data_line[:-1]
 
-    for key, value in d_sensors.items():
-        sensors_data_line += key + "=?,"
+    n = len(sql_dict)
+    values_str = ',?' * len(sql_dict)
+    if is_one_row:
+        values_str = "1" + values_str
+        data_line = "ID," + data_line
+    else:
+        values_str = values_str[1:]
 
-    sensors_data_line = sensors_data_line[:-1]
-    print(sensors_data_line)
     curs.execute('''
-                UPDATE dbo.SensorsInfo
-                SET ''' + sensors_data_line + '''
-                WHERE ID = 1 ''', data)
+                   IF NOT EXISTS (SELECT * FROM  dbo.''' + table + ''' WHERE ID = 1) 
+                       INSERT INTO dbo.'''+table + '''(''' + data_line + ''')
+                       VALUES(''' + values_str + ''')
+                    ''', initial_info
+                 )
+
+    conn.commit()
+
+
+def update_sql(curs, conn, table, data, is_one_row, sql_dict):
+    data_line = ""
+
+    if is_one_row:
+        for key, value in sql_dict.items():
+            data_line += key + "=?,"
+        data_line = data_line[:-1]
+
+        curs.execute('''
+                    UPDATE dbo.''' + table + '''
+                    SET ''' + data_line + '''
+                    WHERE ID = 1 ''', data)
+    else:
+        for key, value in sql_dict.items():
+            data_line += key + ","
+        data_line = data_line[:-1]
+
+        n = len(sql_dict)
+        values_str = ',?' * len(sql_dict)
+        values_str = values_str[1:]
+
+        curs.execute('''
+                        INSERT INTO dbo.''' + table + '''(''' + data_line + ''')
+                        VALUES(''' + values_str + ''')
+                        ''', data
+                     )
 
     conn.commit()
 
 
 ## printing updated sql data in a dict way (Column, row). optional to choose table name
-def print_sql_row(curs ,table_name = "SensorsInfo"):
+def print_sql_row(curs, table_name="SensorsInfo"):
     table_rows = curs.execute("SELECT * FROM " + table_name)
     columns = [column[0] for column in curs.description]
     print(columns)
--- sql_initiate.py
+++ sql_initiate.py
@@ -11,16 +11,20 @@ if __name__ == '__main__':
     server = 'localhost'
     username = 'sa'
     password = 'ItamarOrel1995'
-    db_connection_string = "Driver={FreeTDS};Server=" + server + ";port=1433" + ";UID=" + username + ";PWD=" + password + ";"
+    db_connection_string = "Driver={ODBC Driver 17 for SQL Server};Server=" + server + ";port=1433" + ";UID=" + username + ";PWD=" + password + ";"
 
     conn = pyodbc.connect(db_connection_string,  autocommit=True)
     cursor = conn.cursor()
 
     init_database(cursor, conn)
-    init__sensors_table(cursor, conn)
+    # drop_table(cursor, conn, "SensorsInfo","SensorsInfoHistory")
 
+    init_sql_table(cursor, conn, "SensorsInfo", d_sensors, True, "SensorsInfoHistory")
+
+    # update_sql(cursor, conn, "SensorsInfo", (str(1), str(5), str(3), str(4)), True, d_sensors)
+    
     print_sql_row(cursor, "SensorsInfo")
-
+    print_sql_row(cursor, "SensorsInfoHistory")
 
 
 
